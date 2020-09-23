ec212979aa8c80536ffc88a5384b8f9c794cd299
ChoChihTun <chochihtun@gmail.com>
2020-09-23 17:25:41 +0800

    Updates

---
+++ precache-manifest.6baa9c9a07a92289bdf6a95737c17625.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f782f866a9ff275f3f9135b964be7a79",
+    "url": "/ui-todo-tutorial/index.html"
+  },
+  {
+    "revision": "191c9c078b0db8eb7908",
+    "url": "/ui-todo-tutorial/static/css/main.08700ed6.chunk.css"
+  },
+  {
+    "revision": "013421d58166b5936b69",
+    "url": "/ui-todo-tutorial/static/js/2.5f8c33bc.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/ui-todo-tutorial/static/js/2.5f8c33bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "191c9c078b0db8eb7908",
+    "url": "/ui-todo-tutorial/static/js/main.0e4d06c5.chunk.js"
+  },
+  {
+    "revision": "ff9ff1abb2c1f2c1bfcf",
+    "url": "/ui-todo-tutorial/static/js/runtime-main.86cf155b.js"
+  }
+]);
